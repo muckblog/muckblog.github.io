cd563427e71bb8cb1e80bc8d65af620b13b529e7
muckblog <59802149+muckblog@users.noreply.github.com>
2023-07-24 22:24:48 +0100

    Updated js to work on all Lego elements

--- assets/js/lego.js
+++ assets/js/lego.js
@@ -1,42 +1,48 @@
 var isDown = false;
 let currentDiv;
 
-const div = document.getElementByClassName("bg-lego");
+const bgLegoElements = document.getElementsByClassName("bg-lego");
 
-const mySounds = [ 'lego-drop1', 'lego-drop2', 'lego-drop3', 'lego-drop4' ];
+const mySounds = ['lego-drop1', 'lego-drop2', 'lego-drop3', 'lego-drop4'];
 
-div.addEventListener('mousedown', function(e) {
-      document.getElementById('lego-pickup').play();
-      isDown = true;
-      const offset = [
-          div.offsetLeft - e.clientX,
-          div.offsetTop - e.clientY
-      ];
-      div.setAttribute('data-offset', JSON.stringify(offset));
-      currentDiv = div;
-}, true);
+function handleMouseDown(e) {
+  document.getElementById('lego-pickup').play();
+  isDown = true;
+  const offset = [
+    this.offsetLeft - e.clientX,
+    this.offsetTop - e.clientY
+  ];
+  this.setAttribute('data-offset', JSON.stringify(offset));
+  currentDiv = this;
+}
 
-div.addEventListener('mouseup', function() {
-    isDown = false;
-    randomSound();
-}, true);
+function handleMouseUp() {
+  isDown = false;
+  randomSound();
+}
 
-div.addEventListener('mousemove', function(event) {
-    event.preventDefault();
-    if (isDown && currentDiv) {
-    const mousePosition = {    
-            x : event.clientX,
-            y : event.clientY    
-        };
-        const offset = JSON.parse(currentDiv.getAttribute('data-offset'));
-        currentDiv.style.left = (mousePosition.x + offset[0]) + 'px';
-        currentDiv.style.top  = (mousePosition.y + offset[1]) + 'px';
-    }
-}, true);
+function handleMouseMove(event) {
+  event.preventDefault();
+  if (isDown && currentDiv) {
+    const mousePosition = {
+      x: event.clientX,
+      y: event.clientY
+    };
+    const offset = JSON.parse(currentDiv.getAttribute('data-offset'));
+    currentDiv.style.left = (mousePosition.x + offset[0]) + 'px';
+    currentDiv.style.top = (mousePosition.y + offset[1]) + 'px';
+  }
+}
+
+for (const div of bgLegoElements) {
+  div.addEventListener('mousedown', handleMouseDown, true);
+  div.addEventListener('mouseup', handleMouseUp, true);
+  div.addEventListener('mousemove', handleMouseMove, true);
+}
 
 function randomSound() {
-      var index = Math.floor(Math.random() * 1000) % mySounds.length;
-      var id = mySounds[index];
-      var audioElement = document.getElementById(id);
-      audioElement.play();
-};
+  var index = Math.floor(Math.random() * 1000) % mySounds.length;
+  var id = mySounds[index];
+  var audioElement = document.getElementById(id);
+  audioElement.play();
+}
